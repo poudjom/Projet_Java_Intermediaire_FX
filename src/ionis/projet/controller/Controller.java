e01df302042cc522fc705f53a11e19adc9736c7c
pc <poudjom@gmail.com>
2021-01-01 15:26:42 +0100

    Commit 01/01/2021 Commit avant le merge

--- src/ionis/projet/controller/Controller.java
+++ src/ionis/projet/controller/Controller.java
@@ -7,14 +7,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Controller {
+
     public void onButton(ActionEvent actionEvent) throws IOException {
         Stage ecranMatiere = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("../vue/jfxmatiere.fxml"));
         ecranMatiere.setTitle("Matière");
         ecranMatiere.setScene(new Scene(root, 700, 500));
         ecranMatiere.show();
+//        String d1,d2;
+//
+//        DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+//        Date date = new Date();
+//        d1 = format.format(date) + " 15:10";
+//        d2 = format.format(date) + " 17:10";
+//        Date
+//        LocalDate date1 = LocalDate.parse(d1);
+//        LocalDate date2 = LocalDate.parse(d2);
+//        long diff = date1.get - date2.getTime();
+//        long diffHours = diff/(60 **  60 **  1000) % 24;
+//        System.out.println(d2);
     }
 
     public void onMiMatiere(ActionEvent actionEvent) throws IOException {
